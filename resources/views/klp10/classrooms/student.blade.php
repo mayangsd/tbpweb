514228613ee53a984a47dabe217b1a1e5c0ae5c5
Mayang Sari Deyuvi <49848014+mayangsd@users.noreply.github.com>
2020-06-27 23:56:24 +0700

    Update student.blade.php

--- resources/views/klp10/classrooms/student.blade.php
+++ resources/views/klp10/classrooms/student.blade.php
@@ -23,42 +23,7 @@
 
                 {{-- CARD HEADER--}}
                 <div class="card-header">
-
-                    <strong><i class="cil-zoom"></i> Tambah Mahasiswa {{ $classrooms->name }}</strong>
-
                     <strong><i class="cil-plus"></i> Tambah Mahasiswa {{ $classrooms->name }}</strong>
-
-
-                </div>
-
-                {{-- CARD BODY--}}
-                <div class="card-body">  
-
-                    <div class="form-group">
-                        <label class="form-label" for="student_id">Nama Mahasiswa</label>
-                        {{ html()->select('student_id')->options($students)->class(["form-control", "is-invalid" => $errors->has('student_id')])->id('student_id')->placeholder('Pilih Mahasiswa') }}
-                        @error('student_id')
-                        <div class="invalid-feedback">{{ $errors->first('student_id') }}</div>
-                        @enderror
-                     </div>
-                </div>
-
-                {{--CARD FOOTER--}}
-                <div class="card-footer">
-                    <input type="submit" value="Simpan" class="btn btn-primary"/>
-                 </div>
-
-                {{ html()->form()->close() }}
-            </div>
-        </div>
-    </div>
-
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                {{-- CARD HEADER--}}
-                <div class="card-header">
-                    <strong><i class="cil-zoom"></i> Daftar Mahasiswa Pada Kelas {{ $classrooms->name }}</strong>
                 </div>
 
                 {{-- CARD BODY--}}
@@ -83,7 +48,6 @@
                     </div>
                  {{-- CARD BODY--}}
                     <div class="card-body">  
-
                     <table class="table table-outline table-hover">
                         <thead class="thead-light">
                              <tr>
@@ -92,23 +56,6 @@
                                 <th>Aksi</th>
                             </tr>
                         </thead>
-<
-                        <tbody>
-                        @forelse($course_selection as $course_selections)
-                        <tr>
-                            <td>{{ $course_selections->student_semesters->students->name }}</td>
-                            <td>{{ $course_selections->student_semesters->students->nim }}</td>
-                            <td>{!! cui()->btn_delete(route('backend.classrooms.students.destroy', [$classrooms->id, $course_selections->id]), "Anda yakin akan menghapus data mahasiswa ini?") !!}</td>
-                        </tr>
-                        @empty
-                        <tr>
-                            <td colspan="5">Belum ada Mahasiswa</td>
-                        </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
-                </div>
-
                     <tbody>
                     @forelse($course_selection as $course_selections)
                     <tr>
@@ -127,19 +74,11 @@
                  @endforelse
                 </tbody>
             </table>
-
             </div>
              
         </div>
 
     </div>
-@endsection
-    
-
-
-
-    </div>
     
 
 @endsection
-
