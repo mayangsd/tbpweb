59c6c8dda40de2c76df5f311f83d76ea9c744ff6
Mayang Sari Deyuvi <49848014+mayangsd@users.noreply.github.com>
2020-06-27 23:57:05 +0700

    Update form.blade.php

--- resources/views/klp10/classrooms/form.blade.php
+++ resources/views/klp10/classrooms/form.blade.php
@@ -1,10 +1,17 @@
+<!-- Text Field Input for Nama Kelas -->
+
 <div class="form-group">
     
-    <label class="form-label" for="student_id">Nama Mahasiswa</label>
-    {{ html()->select('student_id')->options($students)->class(["form-control", "is-invalid" => $errors->has('student_id')])->id('student_id')->placeholder('Pilih Id Mahasiswa') }}
-    @error('student_id')
-    <div class="invalid-feedback">{{ $errors->first('student_id') }}</div>
+    <label class="form-label" for="student_semester_id">Nama Mahasiswa</label>
+    {{ html()->select('student_semester_id')->options($students)->class(["form-control", "is-invalid" => $errors->has('student_semester_id')])->id('student_semester_id')->placeholder('Nama Mahasiswa') }}
+    @error('student_semester_id')
+    <div class="invalid-feedback">{{ $errors->first('student_semester_id') }}</div>
     @enderror
 
+    <!-- <label class="form-label" for="classroom_id">Classroom</label>
+    {{ html()->select('classroom_id')->options($classrooms)->class(["form-control", "is-invalid" => $errors->has('classroom_id')])->id('classroom_id')->placeholder('Classroom') }}
+    @error('classroom_id')
+    <div class="invalid-feedback">{{ $errors->first('classroom_id') }}</div>
+    @enderror -->
 
 </div>
